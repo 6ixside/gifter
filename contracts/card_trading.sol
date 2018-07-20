3b9a5b321da73639e1f5abe5aa3adca5b151aa69
Connor Dear <connordear@gmail.com>
2018-07-20 01:17:29 -0400

    added new trading logic...

--- contracts/card_trading.sol
+++ contracts/card_trading.sol
@@ -4,14 +4,38 @@ import "./card_factory.sol";
 
 contract CardTrading is CardFactory {
 
-    event tradeSuccess(address playerOne, address playerTwo, uint256 cardIdOne, uint256 cardIdTwo);
+    mapping (uint => address) tradeApprovals;
 
-    function trade(address _playerTwo, uint256 _cardIdOne,
-        uint256 _cardIdTwo) public {
-            require(cardToOwner[_cardIdOne] == msg.sender);
-            require(cardToOwner[_cardIdTwo] == _playerTwo);
+    modifier onlyOwnerOf(uint _cardId) {
+      require(msg.sender == cardToOwner[_cardId]);
+      _;
+    }
 
-            cardToOwner[_cardIdOne] = _playerTwo;
-            cardToOwner[_cardIdTwo] = msg.sender;
-        }
+    event Transfer(address indexed _from, address indexed _to, uint256 indexed cardId);
+
+    event Approval(address indexed _from, address indexed _to, uint256 indexed cardId);
+
+    function ownerOf(uint256 _cardId) public view returns (address _owner) {
+        return cardToOwner[_cardId];
+    }
+
+    function transfer(address _to, uint256 _cardId) public onlyOwnerOf(_cardId){
+        _transfer(msg.sender, _to, _cardId);
+    }
+
+    function approve(address _to, uint256 _cardId) public onlyOwnerOf(_cardId){
+        tradeApprovals[_cardId] = _to;
+        emit Approval(msg.sender, _to, _cardId);
+    }
+
+    function takeOwnership(uint256 _cardId) public {
+        require(tradeApprovals[_cardId] == msg.sender);
+        address owner = ownerOf(_cardId);
+        _transfer(owner, msg.sender, _cardId);
+    }
+
+    function _transfer(address _from, address _to, uint256 _cardId) private {
+          cardToOwner[_cardId] = _to;
+          emit Transfer(_from, _to, _cardId);
+    }
 }
