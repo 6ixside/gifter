4f244e50780cb0a703ce291f63e8fe2e35327b96
Connor Dear <connordear@gmail.com>
2018-07-19 23:22:03 -0400

    fixed inheritance into trading

--- contracts/card_trading.sol
+++ contracts/card_trading.sol
@@ -1,16 +1,17 @@
 pragma solidity ^0.4.19;
 
-import "./card_factory";
+import "./card_factory.sol";
 
-contract CardTrading {
-    
+contract CardTrading is CardFactory {
+
     event tradeSuccess(address playerOne, address playerTwo, uint256 cardIdOne, uint256 cardIdTwo);
-    
+
     function trade(address _playerTwo, uint256 _cardIdOne,
         uint256 _cardIdTwo) public {
             require(cardToOwner[_cardIdOne] == msg.sender);
             require(cardToOwner[_cardIdTwo] == _playerTwo);
+
             cardToOwner[_cardIdOne] = _playerTwo;
-            cardToOwner[_cardIdTwo] = _playerOne;
+            cardToOwner[_cardIdTwo] = msg.sender;
         }
 }
