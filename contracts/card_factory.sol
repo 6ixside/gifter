487873e1bc3fb23ba81d08edc5601dd19c7bfd03
Connor Dear <connordear@gmail.com>
2018-07-20 01:19:06 -0400

    got rid of safemath, cleaned up cardFactory

--- contracts/card_factory.sol
+++ contracts/card_factory.sol
@@ -5,23 +5,18 @@ import "./ownable.sol";
 
 contract CardFactory is Ownable {
 
-    using SafeMath for uint256;
-    using SafeMath32 for uint32;
-    using SafeMath16 for uint16;
-    
     event NewCard(uint cardId, string company, uint dna);
-    
+
     struct Card {
         string company;
         uint16 balance;
     }
-    
+
     Card[] public cards;
-    
+
     mapping (uint => address) public cardToOwner;
-    mapping (address => uint) ownerCardCount;
-    
-    
+    mapping (address => uint) ownerCardCount;  
+
     function createCard(string company, uint16 balance) public onlyOwner {
         uint id = cards.push(Card(company, balance));
         cardToOwner[id] = msg.sender;
