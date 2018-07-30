6741a54f1d44d080efa20964871bbf64a665d8f6
Connor Dear <connordear@gmail.com>
2018-07-29 20:01:51 -0400

    updated card_factory and card_trading contracts to allow proper trading

--- contracts/card_trading.sol
+++ contracts/card_trading.sol
@@ -4,40 +4,59 @@ import "./card_factory.sol";
 
 contract CardTrading is CardFactory {
 
-    mapping (uint => address) tradeApprovals;
-
+    /* For cases when functions need to be restricted to owners */
     modifier onlyOwnerOf(uint _cardId) {
       require(msg.sender == cardToOwner[_cardId]);
       _;
     }
 
-    event Transfer(address indexed _from, address indexed _to, uint256 indexed cardId);
+    /* Ensure that the card is up for trade */
+    modifier onlyTrading(uint _cardId) {
+      require(isTrading[_cardId]);
+      _;
+    }
 
-    event Approval(address indexed _from, address indexed _to, uint256 indexed cardId);
+    /* Ensure that the card being sent for the trade is what the user requested */
+    modifier onlyValidOffer(uint _cardOne, uint _cardTwo) {
+      require(keccak256(abi.encodePacked(tradingFor[_cardOne])) == keccak256(abi.encodePacked(cards[_cardTwo].company)));
+      _;
+    }
+
+    event Transfer(address indexed _from, address indexed _to, uint256 indexed cardId);
 
     function ownerOf(uint256 _cardId) public view returns (address _owner) {
         return cardToOwner[_cardId];
     }
 
-    function transfer(address _to, uint256 _cardId) public onlyOwnerOf(_cardId){
-        _transfer(msg.sender, _to, _cardId);
+    function openTrading(uint256 _cardId, string company) public onlyOwnerOf(_cardId) {
+      isTrading[_cardId] = true;
+      tradingFor[_cardId] = company;
     }
 
-    function approve(address _to, uint256 _cardId) public onlyOwnerOf(_cardId){
-        tradeApprovals[_cardId] = _to;
-        emit Approval(msg.sender, _to, _cardId);
+    function closeTrading(uint256 _cardId) public onlyOwnerOf(_cardId) {
+      isTrading[_cardId] = false;
+      tradingFor[_cardId] = '';
     }
 
-    function takeOwnership(uint256 _cardId) public {
-        require(tradeApprovals[_cardId] == msg.sender);
-        address owner = ownerOf(_cardId);
-        _transfer(owner, msg.sender, _cardId);
+    /* Make Trade
+    _cardOne: ID of the card open for Trade
+    _cardTwo: msg.sender's card fulfilling the trade
+     */
+    function makeTrade(uint256 _cardOne, uint256 _cardTwo) public onlyOwnerOf(_cardTwo) onlyTrading(_cardOne) onlyValidOffer(_cardOne, _cardTwo){
+        _swapCards(cardToOwner[_cardOne], cardToOwner[_cardTwo], _cardOne, _cardTwo);
     }
 
-    function _transfer(address _from, address _to, uint256 _cardId) private {
+
+    /* Internal functions for performing the transfer of ownership */
+    function _transfer(address _from, address _to, uint256 _cardId) private onlyOwnerOf(_cardId){
           cardToOwner[_cardId] = _to;
-          ownerCardCount[_from]--;
-          ownerCardCount[_from]++;
           emit Transfer(_from, _to, _cardId);
     }
+
+    function _swapCards(address _playerOne, address _playerTwo, uint _cardOne, uint _cardTwo) private {
+      _transfer(_playerOne, _playerTwo, _cardOne);
+      _transfer(_playerTwo, _playerOne, _cardTwo);
+    }
+
+
 }
