e6fd08a968f0992f2009bf98bcc953b52e3d6e5a
mromao <mromao@uwo.ca>
2018-08-06 20:04:42 -0400

    Added callback route to shopify plugin to accept permanent access token

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.get('/shopify', (req,res) => {
+app.get('/shopify', (req, res) => {
     const shop = req.query.shop;
     if (shop) {
         const state = nonce();
@@ -32,6 +32,50 @@ app.get('/shopify', (req,res) => {
 
     else
         return res.status(400).send("Missing shop parameter. Please add ?shop=giftersixside.myshopify.com to your request"); 
+});
+
+app.get('/shopify/callback', (req, res) => {
+    const {shop, hmac, code, state} = req.query;
+    const stateCookie = cookie.parse(req.headers.cookie).state;
+
+    if(state != stateCookie)
+        return res.status(403).send("Request origin cannot be verified.");
+
+    if(shop && hmac && code) {
+        const map = Object.assign({}, req.query);
+        delete map["signature"];
+        delete map["hmac"];
+        const message = querystring.stringify(map);
+        const providedHmac = Buffer.from(hmac, "utf-8");
+        const generateHash = Buffer.from(
+            crypto
+            .createHmac("sha256", apiSecret)
+            .update(message)
+            .digest("hex"), 
+            "utf-8"
+        );
+
+        let hashEquals = false;
+
+        try {
+            hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
+        }
+
+        catch(e) {
+            hashEquals = false;
+        }
+
+        if(!hashEquals) {
+            return res.status(400).send("Hmac validation failed.");
+        }
+
+        res.status(200).send("Hmac validated.");
+    }
+
+    else {
+        res.status(400).send("Required parameters missing.");
+    }
+
 });
 
 app.listen(3000, () => {
